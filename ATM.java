2952e086960d3a2b8090db23871c08099d6ee57b
ReeveshSevar <110165835+ReeveshSevar@users.noreply.github.com>
2022-07-28 15:19:14 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,24 @@
+package atm;
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+		optionMenu.getLogin();
+	
+		/* The Valid Account Numbers And Pin Numbers Are Given Below
+		  AccNo    |   PinNo
+		  __________________
+		  27201    |   0001
+		  27202    |   0002
+		  27203    |   0003
+		  27204    |   0004
+		  27205    |   0005
+		  27206    |   0006
+		  27207    |   0007
+		 */
+		
+		
+	}
+}
